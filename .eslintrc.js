fc0e2ba5b12f00db7a096b7a2ddcb2242a2e1d88
rudenkoserhii <rudenko.sergey.v@gmail.com>
2024-03-16 13:20:00 +0200

    feat: added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['react', 'import', 'jsx-a11y', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
