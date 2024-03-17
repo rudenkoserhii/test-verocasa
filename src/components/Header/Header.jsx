438864f3bb3e2affdd0fe9cb4ebf176b31582b3c
rudenkoserhii <rudenko.sergey.v@gmail.com>
2024-03-17 09:34:14 +0200

    feat: added header upper

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
+import 'components/Header/header.scss'
+import { CONTACTS } from 'consts'
 
 export const Header = () => (
   <header>
     <div className="header__upper">
-      <div></div>
-      <ul>
-        <li>
-          <a href="/login">Увійти</a>
+      <div className="header__help">
+        <a className="link header__link header__link--tel" href={`tel:${CONTACTS.tel}`}>
+          {CONTACTS.tel}
+        </a>
+        <a
+          className="link header__link header__link--help"
+          href="/help"
+          target="_blank"
+          rel="noopener"
+        >
+          Допомога
+        </a>
+      </div>
+      <ul className="list header__signs">
+        <li className="header__sign">
+          <a
+            className="link header__link header__link--sign"
+            href="/login"
+            target="_blank"
+            rel="noopener"
+          >
+            Увійти
+          </a>
+        </li>
+        <span className="header__link--sign">{'/'}</span>
+        <li className="header__sign">
+          <a
+            className="link header__link header__link--sign"
+            href="/signup"
+            target="_blank"
+            rel="noopener"
+          >
+            Зареєструватися
+          </a>
         </li>
       </ul>
     </div>
