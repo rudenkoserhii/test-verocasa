2f9c73876560ea8bcf1d4f1fe6c89aa3cd0cf64c
rudenkoserhii <rudenko.sergey.v@gmail.com>
2024-03-17 22:08:01 +0200

    feat: made component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Hero } from 'components/Hero/Hero'
 import { News } from 'components/News/News'
+import { Top } from 'components/Top/Top'
+import { Subscription } from 'components/Subscription/Subscription'
+import { Opinions } from 'components/Opinions/Opinions'
 
 const Home = () => (
   <>
     <Hero />
     <News />
+    <Top />
+    <Subscription />
+    <Opinions />
   </>
 )
 
